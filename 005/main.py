f111ff8e73103dcc2f6e7dc4f1e1e06783507729
Mehran Tarif <mrmntf@gmail.com>
2025-11-02 19:39:30 +0100

    session 5

---
+++ 005/main.py
@@ -0,0 +1,13 @@
+from cgitb import reset
+from fastapi import FastAPI
+from schemas import UserRequest, UserResponse
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/user", response_model=UserResponse)
+def create_user(user: UserRequest):
+    return {"message": "User created", "user": user}
